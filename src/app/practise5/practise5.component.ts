54ae90e006895006934b98f0fb7bc8d5cfe296b7
羊羊 <90090207as@gmail.com>
2025-04-10 15:03:49 +0800

    歐咩嘎是練習5

--- src/app/practise5/practise5.component.ts
+++ src/app/practise5/practise5.component.ts
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './practise5.component.html',
   styleUrl: './practise5.component.scss'
 })
-export class Practise5Component {
-
+export class Practise5Component
+{
+  /** 用於h1標籤的字串 */
+  h1_text: string = "我是標題";
+  /** 用於h3標籤的字串 */
+  h3_text: string = "下面是動物森友會的各種小可愛們";
+  /** 圖片連結 */
+  img_src: string = "/littlelamb_mero/AnimalCrossing Rosie Pixiv 01.jpg"
 }
