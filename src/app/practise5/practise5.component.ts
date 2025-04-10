f2c69352aef8bfa5b8f2e98c6a24edbcc3e24bcf
s6221339 <s6221339@yahoo.com.tw>
2025-04-10 17:38:51 +0800

    練習五

--- src/app/practise5/practise5.component.ts
+++ src/app/practise5/practise5.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './practise5.component.scss'
 })
 export class Practise5Component {
-
+  title: string='我是標題'
+  content: string='我是內容'
+  img: any='https://upload.wikimedia.org/wikipedia/zh/5/5b/Pokemon_Anime_Sun%26Moon.jpg'
 }
