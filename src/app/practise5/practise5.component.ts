eb245c506ba944e458c940e3ba806bab5dd6382c
Yi <kl881221@gmail.com>
2025-04-10 16:15:53 +0800

    練習五

--- src/app/practise5/practise5.component.ts
+++ src/app/practise5/practise5.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './practise5.component.scss'
 })
 export class Practise5Component {
-
+title:string = '我是標題'
+content: string = '我是內容'
+img:string = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRjRWkFqmWlziUIRgC-MvEYfDq75t9BfukMEWfWgNV46buT1T4EdKyCJOVeotJ6FcZ3mg&usqp=CAU'
 }
