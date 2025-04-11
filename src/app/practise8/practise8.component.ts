8671be095c7132bc2c8af08744df8d0e8a84447f
Hsin <mc881221@gmail.com>
2025-04-11 10:56:07 +0800

    練習八

--- src/app/practise8/practise8.component.ts
+++ src/app/practise8/practise8.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Component1Component } from '../component1/component1.component';
+import { Component2Component } from '../component2/component2.component';
 
 @Component({
   selector: 'app-practise8',
   standalone: true,
-  imports: [],
+  imports: [Component1Component, Component2Component],
   templateUrl: './practise8.component.html',
   styleUrl: './practise8.component.scss'
 })
