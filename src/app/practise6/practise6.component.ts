d859d7290d5fa97dfcd5988f5430c693d1e1cfd4
Chad <nyanpasu1998@gmail.com>
2025-04-11 14:48:48 +0800

    練習6

--- src/app/practise6/practise6.component.ts
+++ src/app/practise6/practise6.component.ts
@@ -23,18 +23,26 @@ export class Practise6Component {
   // 練習1
   // 試著將1~50打印出來。(for迴圈)
   practise1() {
+    for (let i = 0; i <= 50; i++) {
+      console.log(i);
+    }
   }
 
   // 練習2
   // 承接第一題將偶數去除並且打應出來。
   practise2() {
-
+    for (let i = 1; i <= 50; i += 2) {
+      console.log(i);
+    }
   }
 
   // 練習3
   // 將1~50除以3餘數為2的值打印出來。(for迴圈)
   practise3() {
-
+    for (let i = 0; i <= 50; i++) {
+      if (i % 3 == 2)
+        console.log(i);
+    }
   }
 
   // 練習4
@@ -54,23 +62,43 @@ export class Practise6Component {
         age: 16
       }
     ]
-  }
+    data.forEach((item, index, array) => {
+      if (item.name == "小王") {
+        item.age = 18;
+      }
+    })
+    console.log(data);
+  };
 
   // 練習5
   // 找出陣列裡的最小值
   practise5() {
     let data = [4, 6, 1, 2, 7, 9, 12, 15, 13];
+    let answer = data[0];
+    for (let i = 0; i < data.length; i++) {
+      if (data[i] < answer) {
+        data[i] = answer;
+      }
+    }
+    console.log(answer);
   }
 
   // 練習6
   // 倒著印出字串
   practise6() {
     let data = '你好我是Allen';
+    let answer = "";
+    for (let i = data.length - 1; i >= 0; i--) {
+      answer = answer + data[i];
+    }
+    console.log(answer);
   }
 
   // 練習7
   // 將這段文字中的名稱打印出來
   practise7() {
     let data = '我是Allen，你好';
+    let answer = data.slice(data.indexOf("Allen"), data.indexOf("Allen") + 5);
+    console.log(answer);
   }
 }
