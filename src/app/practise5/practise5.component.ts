d5e3fd466005f697902f5aaa079ce4ea3c5753e4
Chad <nyanpasu1998@gmail.com>
2025-04-11 10:20:34 +0800

    練習5

--- src/app/practise5/practise5.component.ts
+++ src/app/practise5/practise5.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './practise5.component.scss'
 })
 export class Practise5Component {
-
+  title: string = "我是標題";
+  content: string = "我是內容";
+  img: string = "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ3Hv6iGG5mrI76aruo7gRz7BTVzMs1SV2wo6hi-ohzY7bF6bxFCYLTqNsCO8GdB8jt2lHehNqcpDfRNYEJ-utWtQ"
 }
