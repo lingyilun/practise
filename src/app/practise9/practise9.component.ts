e8a0b26027498fa496a9de87ba0664cbf95e0d65
Yi <kl881221@gmail.com>
2025-04-11 16:12:41 +0800

    練習九

--- src/app/practise9/practise9.component.ts
+++ src/app/practise9/practise9.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-practise9',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './practise9.component.html',
   styleUrl: './practise9.component.scss'
 })
 export class Practise9Component {
+level:number = 1;
+attack:number = 10;
+defense:number = 10;
+inputLevel!:number;
+
+reset(){
+  this.level = 1
+  this.attack = 10
+  this.defense = 10
+}
+
+levelUp(){
+  this.level = this.level + 1
+  this.attack = this.attack * 3
+  this.defense = this.defense * 2
+}
+
+levelDown(){
+  if(this.level != 1 && this.attack != 10 && this.defense != 10){
+  this.level = this.level - 1
+  this.attack = this.attack / 3
+  this.defense = this.defense / 2
+  }
+}
+
+changeLevel(){
+  this.reset()
+  for(let i = 1; i < this.inputLevel; i++ ){
+    this.levelUp()
+  }
+
+}
+
+
 
 }
