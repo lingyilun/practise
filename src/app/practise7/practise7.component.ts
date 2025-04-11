d91d53b757d9b757f028c8e020c36ef20058df2d
Chad <nyanpasu1998@gmail.com>
2025-04-11 16:02:53 +0800

    練習7

--- src/app/practise7/practise7.component.ts
+++ src/app/practise7/practise7.component.ts
@@ -14,7 +14,7 @@ export class Practise7Component {
     // this.practise1();
     // this.practise2();
     // this.practise3();
-    // this.practise4();
+    this.practise4();
   }
 
   // 練習1
@@ -23,6 +23,8 @@ export class Practise7Component {
     let allenMoney = 200;
     let hamburger = 50;
     let fries = 40;
+    let answer = allenMoney - (hamburger + (fries * 3))
+    console.log(answer);
   }
 
   // 練習2
@@ -31,6 +33,9 @@ export class Practise7Component {
     let allenMoney = 5000;
     let hamburger = 50;
     let fries = 40;
+    let membership = 0.9;
+    let answer = allenMoney - membership * (hamburger * 10 + fries * 10);
+    console.log(answer);
   }
 
   // 練習3
@@ -50,6 +55,12 @@ export class Practise7Component {
         payMoney: 120
       }
     ]
+    let answer = [];
+    for (let data of jsonData) {
+      if (data.payMoney >= 200)
+        answer.push(data.userName)
+    }
+    console.log(answer);
   }
 
   // 練習4
@@ -59,5 +70,14 @@ export class Practise7Component {
     let height = 165;
     // 體重輸入在這裡
     let weight = 65;
+    let bmi = weight / ((height / 100) * (height / 100));
+    let level = "";
+    if (bmi < 18.5) level = "過輕"
+    else if (bmi >= 18.5 && bmi <= 24) level = "正常"
+    else if (bmi >= 24 && bmi < 27) level = "過重"
+    else if (bmi >= 27 && bmi <= 30) level = "輕度肥胖"
+    else if (bmi >= 30 && bmi <= 35) level = "中度肥胖"
+    else level = "重度肥胖"
+    console.log(level);
   }
 }
